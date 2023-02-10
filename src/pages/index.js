bf18f715eb29e4f8353d12007830b91179669a26
KPorus <designerfardin2021@gmail.com>
2023-02-11 00:11:05 +0600

    name

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       </Head>
       <main>
         <h1>Md. Habibur Rahman Shohel</h1>
-        <h1>Md. fardin khan</h1>
+        <h1 className='uppercase text-center hover:text-white hover:bg-black'>Md. fardin khan</h1>
       </main>
     </>
   )
