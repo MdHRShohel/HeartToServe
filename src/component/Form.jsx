3cd29d03830103a887989a472b1a32eef8b031ef
KPorus <designerfardin2021@gmail.com>
2023-02-11 10:44:12 +0600

    form img

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,19 +1,26 @@
 export default () => {
     return (
-      <div>
-        <div className='relative'>
-          <div className='bg-cover bg-center bg-no-repeat w-full h-[30vh] relative after:bg-black'>
-            <img
-              src='https://i.ibb.co/4MrS1DZ/pexels-rodnae-productions-6646894-1.jpg'
-              alt='volunter'
-            />
+      <div className='container mx-auto'>
+        <div>
+          <div
+            className='max-w-full'
+            style={{
+              background:
+                "linear-gradient(rgb(255 226 218 / 16%), rgb(255 226 218 / 21%)), url(https://i.ibb.co/4MrS1DZ/pexels-rodnae-productions-6646894-1.jpg)",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              height: "550px",
+              width: "100%",
+            }}>
+            <div className='flex justify-center items-center h-[550px]'>
+              <h1 className='uppercase font-bold  text-white text-6xl'>
+                Become a Volunter
+              </h1>
+            </div>
           </div>
-          <h1 className='uppercase font-bold absolute top-28 left-[30%] text-white text-4xl'>
-            BECOme a Volunter
-          </h1>
         </div>
-        <div className='container mx-auto flex gap-2'>
-          <div>requirements</div>
+        <div className='flex gap-2'>
+          <div>Requirements</div>
           <div className='bg-[#ffe2da]'>Form</div>
         </div>
       </div>
