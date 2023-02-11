41ed03b402ade5a7aa13a24178e749cb59390c0b
mahabubr <mahabub.r753@gmail.com>
2023-02-11 16:35:05 +0600

    small banner done

--- src/component/SmallCounterBanner.jsx
+++ src/component/SmallCounterBanner.jsx
@@ -1,29 +1,56 @@
 import { MdBloodtype } from "react-icons/md";
+import { HiEmojiHappy } from "react-icons/hi";
+import { RiHandCoinFill, RiBook2Fill } from "react-icons/ri";
 
 function SmallCounterBanner() {
 	return (
-		<div className='container mx-auto w-10/12'>
+		<div className='mx-auto container'>
 			<div
-				className='blur-3xl px-16 py-8 flex justify-between items-center'
+				className='px-16 py-8 flex justify-between items-center'
 				style={{
-					backgroundImage: `url(https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)`,
+					backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.2)),url(https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)`,
 					height: "200px",
 					backgroundRepeat: "no-repeat",
 					backgroundSize: "cover",
 					backgroundPosition: "center",
 					width: "100%",
-					filter: "brightness(50%)",
 				}}>
 				<div>
-					<span className='flex text-orange-400'>
-						<MdBloodtype />
+					<span className='flex text-orange-400 font-bold text-3xl items-center'>
+						<MdBloodtype fontSize={50} />
 						<p>15875</p>
 					</span>
-					<p>DONATIONS MADE</p>
+					<p className='text-lg text-center font-bold text-white items-center'>
+						DONATIONS MADE
+					</p>
 				</div>
-				<div>Hello</div>
-				<div>Hello</div>
-				<div>Hello</div>
+				<div>
+					<span className='flex text-orange-400 font-bold text-3xl items-center'>
+						<HiEmojiHappy fontSize={50} />
+						<p>65842</p>
+					</span>
+					<p className='text-lg text-center font-bold text-white'>
+						HAPPY CHILDREN
+					</p>
+				</div>
+				<div>
+					<span className='flex text-orange-400 font-bold text-3xl items-center'>
+						<RiHandCoinFill fontSize={50} />
+						<p>5482</p>
+					</span>
+					<p className='text-lg text-center font-bold text-white'>
+						VOLUNTEERING HELPERS
+					</p>
+				</div>
+				<div>
+					<span className='flex text-orange-400 font-bold text-3xl items-center'>
+						<RiBook2Fill fontSize={50} />
+						<p>47895</p>
+					</span>
+					<p className='text-lg text-center font-bold text-white'>
+						EDUCATED CHILDREN
+					</p>
+				</div>
 			</div>
 		</div>
 	);
