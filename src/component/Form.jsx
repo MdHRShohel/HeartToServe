eada116a2e0045e19abb1f8414d0d60d02577601
KPorus <designerfardin2021@gmail.com>
2023-02-11 10:14:54 +0600

    form update

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,15 +1,16 @@
 export default () => {
     return (
       <div>
-        <div className="relative">
-          <img
-            className='h-[300px] w-full relative after:bg-black '
-            width={1280}
-            height={300}
-            src='https://i.ibb.co/sg1jBQ3/become-a-volunter.jpg'
-            alt='volunter'
-          />
-          <h1 className="uppercase font-bold absolute top-28 left-[30%] text-white text-4xl">BECOme a Volunter</h1>
+        <div className='relative'>
+          <div className='bg-cover bg-center bg-no-repeat w-full h-[30vh] relative after:bg-black'>
+            <img
+              src='https://i.ibb.co/4MrS1DZ/pexels-rodnae-productions-6646894-1.jpg'
+              alt='volunter'
+            />
+          </div>
+          <h1 className='uppercase font-bold absolute top-28 left-[30%] text-white text-4xl'>
+            BECOme a Volunter
+          </h1>
         </div>
         <div className='container mx-auto flex gap-2'>
           <div>requirements</div>
