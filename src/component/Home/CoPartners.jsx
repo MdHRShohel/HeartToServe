01d0f6de0513afd0ab532fdff3e2d82ceb3a30a4
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-02-12 04:31:55 +0600

    images added to the CoPartners

--- src/component/Home/CoPartners.jsx
+++ src/component/Home/CoPartners.jsx
@@ -1,26 +1,16 @@
 const CoPartners = () => {
     return (
-       <div>
-            <h1 className="text-center text-orange-500 font-bold text-3xl drop-shadow-lg">OUR CORPORATE PARTNERS</h1>
-            <div className="container">
-                <div>
-                    <img src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award1.svg" alt="" srcset="" />
-                </div>
-                <div>
-                    <img src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award2.svg" alt="" srcset="" />
-                </div>
-                <div>
-                    <img src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award3.svg" alt="" srcset="" />
-                </div>
-                <div>
-                    <img src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award4.svg" alt="" srcset="" />
-                </div>
-                <div>
-                    <img src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award5.svg" alt="" srcset="" />
-                </div>
-                <div>
-                    <img src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award6.svg" alt="" srcset="" />
-                </div>
+        <div className="container">
+           <h1 className="text-center text-orange-500 font-bold text-3xl drop-shadow-lg">OUR CORPORATE PARTNERS</h1>
+            <div className="mt-10 grid lg:grid-cols-4">
+                <img className="fluid" src="https://nvsautomatizacion.com/wp-content/uploads/2017/08/headAntaira.png" alt="" srcset="" />
+                <img className="fluid" src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award1.svg" alt="" srcset="" />
+                <img className="fluid" src="https://upload.wikimedia.org/wikipedia/commons/8/87/Genesis_logo.png" alt="" srcset="" />
+                <img className="fluid" src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award2.svg" alt="" srcset="" />
+                <img className="fluid" src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award3.svg" alt="" srcset="" />
+                <img className="fluid" src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award4.svg" alt="" srcset="" />
+                <img className="fluid" src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award5.svg" alt="" srcset="" />
+                <img className="fluid" src="https://velikorodnov.com/html/globalgive/charity/images/272x96_award6.svg" alt="" srcset="" />
             </div>
        </div>
     );
