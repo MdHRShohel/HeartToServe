6dd83a90b9891a2d8ae1bc850a674315a9b20064
mahabubr <mahabub.r753@gmail.com>
2023-02-18 19:18:18 +0600

    width recover

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${poppins.className} overflow-x-hidden`}>
+      <main className={`${poppins.className} overflow-x-hidden w-11/12 mx-auto`}>
         <Toaster />
         <HomeContainer />
       </main>
